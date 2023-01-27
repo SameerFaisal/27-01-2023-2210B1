0b68ae23ffc039a036bacae43bb16f8b75d3cd5a
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-01-27 12:34:24 +0500

    27-01-2023

---
+++ 27-01-2023/app.js
@@ -0,0 +1,40 @@
+
+// let data={
+//     std1:{
+//         name:"John",
+//         age:16
+//     },
+//     std2:{
+//         name:"David",
+//         age:18,
+//         contact:[03001111111,"03331111111"]
+//     }
+// }
+
+// document.write(data.std2.contact[0])
+
+// let str="Pakistan"
+
+// document.write(str.lastIndexOf("tan"))
+
+// let arr=["s","a","m"]
+// document.write(arr.reverse())
+
+// let numbers=[4,1,0,5,2]
+// document.write("Original Array: "+numbers+"<br>")
+// numbers.sort()
+// document.write("Sorted Array: "+numbers.reverse())
+
+
+// $(document).ready(function(){
+//     $("button").click(function(){
+//       $("div").animate({left: '250px'});
+//     });
+//   });
+
+let str="PAKISTAN"
+
+
+for(let i=str.length-1;i>=0;i--){
+    document.write(str[i])
+}
